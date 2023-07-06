7a9a5840c8008f5802ef853f613ede0e6b6d1512
andrei-229 <andrejbelak708@gmail.com>
2023-07-06 21:59:31 +0300

    update_token_2

--- modules/core/maps.py
+++ modules/core/maps.py
@@ -4,7 +4,8 @@ from modules.feedbacks import (reply_cabinet_handler, scan_cabinet_handler,
 from modules.microsoft.handlers import refresh_ms_token_handler
 from modules.wb_bot.cabinets import (handle_cabinets_add_command,
                                      handle_cabinets_command,
-                                     handle_cabinets_remove_command)
+                                     handle_cabinets_remove_command,
+                                     handle_cabinets_update_command)
 from modules.wb_bot.purchases import (handle_enter_promocode_purchase_command,
                                       handle_subscribe_purchase_command)
 from modules.wb_bot.users import (handle_users_add_command,
@@ -17,7 +18,8 @@ COMMAND_FUNCTION_MAP = {
     'users_remove': handle_users_remove_command,
     'cabinets': handle_cabinets_command,
     'cabinets_add': handle_cabinets_add_command,
-    'cabinets_remove': handle_cabinets_remove_command
+    'cabinets_remove': handle_cabinets_remove_command,
+    'cabinets_update': handle_cabinets_update_command
 }
 
 PURCHASE_COMMAND_FUNCTION_MAP = {
--- modules/wb_bot/cabinets/__init__.py
+++ modules/wb_bot/cabinets/__init__.py
@@ -1,3 +1,4 @@
 from .handlers.add import handler as handle_cabinets_add_command
 from .handlers.remove import handler as handle_cabinets_remove_command
 from .handlers.root import handler as handle_cabinets_command
+from .handlers.update import handler as handle_cabinets_update_command
