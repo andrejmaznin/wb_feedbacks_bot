e654ee18e77d57b9ecfd7feb31dba9c81797bf31
andrejmaznin <andrejmaznin@gmail.com>
2023-07-09 13:03:00 +0500

    error handling

--- modules/feedbacks/handlers.py
+++ modules/feedbacks/handlers.py
@@ -1,4 +1,5 @@
 import json
+import logging
 from typing import Dict
 
 from app.connections import get_reply_queue, get_scan_queue
@@ -14,45 +15,57 @@ from modules.feedbacks.schemas import (ReplyCabinetTask, ReplySettingsSchema,
                                        ScanCabinetTask, UpdateFeedbacksTask)
 from modules.purchases import check_should_execute
 
+logger = logging.getLogger(__name__)
+
 
 def scan_cabinet_handler(message: Dict) -> None:
-    reviews_queue = get_reply_queue()
-    scan_queue = get_scan_queue()
-
-    task = ScanCabinetTask.parse_obj(json.loads(message['details']['message']['body']))
-    cabinet = CabinetSchema.get_by_id(id=task.cabinet_id)
-    if not check_should_execute(client_id=task.client_id) or cabinet.invalid:
-        return
-
     try:
-        reviews = scan_cabinet(cabinet=cabinet, client_id=task.client_id)
-    except WBAuthException:
-        cabinet.mark_as_invalid()
-        notify_invalid_cabinet(cabinet=cabinet)
-        return
+        reviews_queue = get_reply_queue()
+        scan_queue = get_scan_queue()
 
-    reviews_queue.send_message(
-        MessageBody=ReplyCabinetTask(
-            clientId=task.client_id,
-            cabinetId=task.cabinet_id,
-            reviews=reviews
-        ).json(by_alias=True)
-    )
-    scan_queue.send_message(
-        MessageBody=task.json(by_alias=True),
-        DelaySeconds=settings.LOGIC.scan_delay
-    )
+        task = ScanCabinetTask.parse_obj(json.loads(message['details']['message']['body']))
+        cabinet = CabinetSchema.get_by_id(id=task.cabinet_id)
+        if not check_should_execute(client_id=task.client_id) or cabinet.invalid:
+            return
+
+        try:
+            reviews = scan_cabinet(cabinet=cabinet, client_id=task.client_id)
+        except WBAuthException:
+            cabinet.mark_as_invalid()
+            notify_invalid_cabinet(cabinet=cabinet)
+            return
+
+        reviews_queue.send_message(
+            MessageBody=ReplyCabinetTask(
+                clientId=task.client_id,
+                cabinetId=task.cabinet_id,
+                reviews=reviews
+            ).json(by_alias=True)
+        )
+        scan_queue.send_message(
+            MessageBody=task.json(by_alias=True),
+            DelaySeconds=settings.LOGIC.scan_delay
+        )
+
+    except Exception as e:
+        logger.error(f'Error while scanning cabinet {e}', exc_info=True)
+        raise e
 
 
 async def reply_cabinet_handler(message: Dict) -> None:
-    task = ReplyCabinetTask.parse_obj(json.loads(message['details']['message']['body']))
-    settings = await ReplySettingsSchema.get_for_client_async(client_id=task.client_id)
-    await reply_for_cabinet(
-        client_id=task.client_id,
-        cabinet_id=task.cabinet_id,
-        reviews=task.reviews,
-        settings=settings
-    )
+    try:
+        task = ReplyCabinetTask.parse_obj(json.loads(message['details']['message']['body']))
+        settings = await ReplySettingsSchema.get_for_client_async(client_id=task.client_id)
+        await reply_for_cabinet(
+            client_id=task.client_id,
+            cabinet_id=task.cabinet_id,
+            reviews=task.reviews,
+            settings=settings
+        )
+
+    except Exception as e:
+        logger.error(f'Error while replying to feedbacks {e}', exc_info=True)
+        raise e
 
 
 def update_feedbacks_handler(message: Dict) -> None:
--- services/reply_cabinet/main.py
+++ services/reply_cabinet/main.py
@@ -1,6 +1,8 @@
+from app.connections.ydb import dispose_connections_async
 from modules.feedbacks import reply_cabinet_handler
 
 
 async def handler(event, context):
     for message in event['messages']:
         await reply_cabinet_handler(message=message)
+    dispose_connections_async()
